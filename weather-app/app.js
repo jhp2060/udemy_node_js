e5b01e0a1b48c196c03eb53734dad01e36c043d3
jhp2060 <jhp2060@gmail.com>
2020-02-03 20:57:00 +0900

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,33 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/414ae3e15f37932392fe1a82cf1ef155/37.505903, 126.861852?lang=ko&units=si'
+// const url = 'https://api.darksky.net/forecast/414ae3e15f37932392fe1a82cf1ef155/37.505903, 126.861852?lang=ko&units=si'
 
-request({ url: url, json: true }, (error, response)=> {
-    // const data = JSON.parse(response.body)
-    const data = response.body.currently
-    console.log(error)
-    console.log(response.body.daily.data[0].summary)
-    console.log('It is currently '+data.temperature+' degrees out.')
-    console.log('There is a '+data.precipProbability+'% chance of rain.')
-})
+// request({ url: url, json: true }, (error, response)=> {
+//     if (error) {
+//         console.log('Unable to connec to weather service!')
+//     } else if (response.body.error) {   
+//         console.log('Unable to find the location')
+//     } else {
+//        const data = response.body.currently
+//         console.log(response.body.daily.data[0].summary)
+//         console.log('It is currently '+data.temperature+' degrees out.')
+//         console.log('There is a '+data.precipProbability+'% chance of rain.')
 
-// // Geocoding
-// // Address -> Lat/Long -> Weather
-
-// const geoURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Sinchon.json?access_token=pk.eyJ1IjoiamhwMjA2MCIsImEiOiJjazYzcWt5YjUwZXU3M21wcG11ZzZxYXAyIn0.D6pVNdphmgDzuu_8uZkPaA&limit=1'
-
-// request({url:geoURL, json:true}, (error, response)=> {
-//     const data = response.body.features[0]
-//     console.log(data.text + ' @('+data.center[1]+','+data.center[0]+')')
+//     }
 // })
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geoURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Gocheok.json?access_token=pk.eyJ1IjoiamhwMjA2MCIsImEiOiJjazYzcWt5YjUwZXU3M21wcG11ZzZxYXAyIn0.D6pVNdphmgDzuu_8uZkPaA&limit=1'
+
+request({ url: geoURL, json: true }, (error, response) => {
+    if (error) {
+        console.log('something wrong')
+    } else if (!response.body.features) {
+        console.log('no search result matching')
+    } else {
+        const data = response.body.features[0]
+        console.log(data.text + ' @(' + data.center[1] + ',' + data.center[0] + ')')
+    }
+})
