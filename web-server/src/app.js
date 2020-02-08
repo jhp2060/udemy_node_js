a9d943ed812ebeeae729b8dffdeb2f77bc7c21a0
jhp2060 <jhp2060@gmail.com>
2020-02-08 16:54:11 +0900

    dynamic template : using hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,38 @@
 const path = require('path')
 const express= require('express')
 
-
 const app = express()
+
+// Define the paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup hanldebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+
+
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Andrew Mead',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Jinhyeok',
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        msg: 'What can I do 4 u?',
+    })
+})
 
 app.get('/weather', (req, res) => {
     const data ={
@@ -13,7 +41,6 @@ app.get('/weather', (req, res) => {
     }
     res.send(data)
 })
-
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
